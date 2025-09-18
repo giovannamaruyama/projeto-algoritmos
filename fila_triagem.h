2f978b7753f5fc587bf8c9d5766dc77e4aefad06
Ana Luísa Leal <analuisaleal@usp.br>
2025-09-18 23:35:53 +0000

    falta fazer algumas verificações e  implementar as funções  de arquivos

---
+++ fila_triagem.h
@@ -0,0 +1,18 @@
+
+#include "paciente.h"
+
+
+void inicia_fila(FilaTriagem *fila);
+
+int fila_vazia(FilaTriagem *fila);
+
+int fila_cheia(FilaTriagem *fila);
+
+int inserir_paciente_fila(FilaTriagem *fila, Paciente paciente);
+
+Paciente remover_paciente_fila(FilaTriagem *fila);
+
+void mostrar_fila(FilaTriagem *fila);
+
+void salvar_fila(FilaTriagem *fila);
+void carregar_fila(FilaTriagem *fila);
