5d334bb3e641a33a71b52438b22ed73d0f3d2448
Giovanna Maruyama <giovannamaruyama@uol.com.br>
2025-09-23 19:42:22 -0300

    Update historico.h

--- historico.h
+++ historico.h
@@ -7,7 +7,9 @@
     bool historico_vazio(HISTORICO *historico);
     bool historico_addprocedimento(HISTORICO *historico, ITEM *procedimento);
     ITEM* historico_removeprocedimento(HISTORICO *historico);
+    ITEM* historico_consultar_ultimo(HISTORICO *historico);
     void historico_imprimir(HISTORICO *historico);
     void historico_apagar(HISTORICO **historico);
+
 
     
